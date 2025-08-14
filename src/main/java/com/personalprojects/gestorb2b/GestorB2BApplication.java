baf3c016372553fbf359902ef34b789e4e496bc8
Kaike <kaikedias98@gmail.com>
2025-08-14 14:58:05 -0300

    Initial commit

---
+++ src/main/java/com/personalprojects/gestorb2b/GestorB2BApplication.java
@@ -0,0 +1,13 @@
+package com.personalprojects.gestorb2b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorB2BApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestorB2BApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/personalprojects/gestorb2b/GestorB2BApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalprojects.gestorb2b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorB2BApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
